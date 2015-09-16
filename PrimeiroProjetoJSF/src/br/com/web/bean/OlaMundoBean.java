0ace8cec882ca06959fb22884bc08830c2cad53f
Cleanderson Lobo <cleandersonlobo@gmail.com>
2015-09-16 03:44:39 -0300

    Primeiro Commit

---
+++ PrimeiroProjetoJSF/src/br/com/web/bean/OlaMundoBean.java
@@ -0,0 +1,19 @@
+package br.com.web.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaMundoBean {
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void enviar() {
+		this.nome = getNome().toUpperCase();
+	}
+}
